c23e6cda660840ee0c7e84643291eebf26280c00
Rudde <13756332+ruddem@users.noreply.github.com>
2020-01-15 14:06:34 +0100

    Update get-shapefiles.sh
    Change 2 addresses to download the polygons.

--- get-shapefiles.sh
+++ get-shapefiles.sh
@@ -20,7 +20,7 @@ tar -xzf data/world_boundaries-spherical.tgz -C data/
 
 # simplified-land-polygons-complete-3857
 echo "downloading simplified-land-polygons-complete-3857..."
-curl -z "data/simplified-land-polygons-complete-3857.zip" -L -o "data/simplified-land-polygons-complete-3857.zip" "http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip"
+curl -z "data/simplified-land-polygons-complete-3857.zip" -L -o "data/simplified-land-polygons-complete-3857.zip" "https://osmdata.openstreetmap.de/download/simplified-land-polygons-complete-3857.zip"
 echo "simplified-land-polygons-complete-3857..."
 unzip $UNZIP_OPTS data/simplified-land-polygons-complete-3857.zip simplified-land-polygons-complete-3857/simplified_land_polygons.shp simplified-land-polygons-complete-3857/simplified_land_polygons.shx simplified-land-polygons-complete-3857/simplified_land_polygons.prj simplified-land-polygons-complete-3857/simplified_land_polygons.dbf simplified-land-polygons-complete-3857/simplified_land_polygons.cpg  -d data/
 
@@ -38,7 +38,7 @@ unzip $UNZIP_OPTS data/ne_10m_populated_places.zip ne_10m_populated_places.shp n
 
 # land-polygons-split-3857
 echo "downloading land-polygons-split-3857..."
-curl -z "data/land-polygons-split-3857.zip" -L -o "data/land-polygons-split-3857.zip" "http://data.openstreetmapdata.com/land-polygons-split-3857.zip"
+curl -z "data/land-polygons-split-3857.zip" -L -o "data/land-polygons-split-3857.zip" "https://osmdata.openstreetmap.de/download/land-polygons-split-3857.zip.zip"
 echo "expanding land-polygons-split-3857..."
 unzip $UNZIP_OPTS data/land-polygons-split-3857.zip land-polygons-split-3857/land_polygons.shp land-polygons-split-3857/land_polygons.shx land-polygons-split-3857/land_polygons.prj land-polygons-split-3857/land_polygons.dbf land-polygons-split-3857/land_polygons.cpg  -d data/
 
